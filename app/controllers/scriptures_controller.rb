e7084dce175be2d33c8c3724e3f6d7562f905691
balaji-jr <balaji.joshua1998@gmail.com>
2020-09-24 11:46:55 +0530

    removed comments

--- app/controllers/scriptures_controller.rb
+++ app/controllers/scriptures_controller.rb
@@ -1,19 +1,17 @@
 class ScripturesController < ApplicationController
   def index
-    # scriptures = Reference.all
-    # render json: scriptures
     reference = params[:ref]
     current_ref = Reference.parse_reference(reference)
-    current_ref[:verses].map do |_verse|
-      render json: Verse.find_by(reference: Reference.find_by(book_name: current_ref[:book_name], chapter_no: current_ref[:chapter_no], verse_no: _verse))
+    verses = []
+    current_ref[:verses].map do |verse|
+      verse_text = Verse.find_by(reference: Reference.find_by(book_name: current_ref[:book_name], chapter_no: current_ref[:chapter_no], verse_no: verse))
+      result_hash = {}
+      result_hash[:book_name] = current_ref[:book_name]
+      result_hash[:chapter_no] = current_ref[:chapter_no]
+      result_hash[:verse] = verse
+      result_hash[:text] = verse_text.verse
+      verses.push(result_hash)
     end
+    render json: verses
   end
-
-  # def show
-  #   byebug
-  #   grade = Grade.find(params[:id])
-  #   render json: grade
-  # end
 end
-
-# Verse.find_by(reference:Reference.find_by(book_name: "Genesis", chapter_no:1, verse_no:2))
